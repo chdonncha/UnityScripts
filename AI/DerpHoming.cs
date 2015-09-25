a498f2893f35de01e8a6db67009c826918cfd0b8
chdonncha <chdonncha@gmail.com>
2015-09-25 11:49:53 +0100

    Added Basic Homing AI

---
+++ AI/DerpHoming.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DerpHoming : MonoBehaviour
+{
+
+    public int rotationSpeed;
+    public int movementSpeed;
+
+    private Transform myTransform;
+    public Transform target;
+
+    // Use this for initialization
+    void Start()
+    {
+        GameObject go = GameObject.FindGameObjectWithTag("Player");
+        target = go.transform;
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveTowardsPlayer();
+    }
+
+    void MoveTowardsPlayer()
+    {
+        //Debug.DrawLine(myTransform.position, target.position, Color.red);
+        //rotate enemy
+        myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+        //move
+        myTransform.position += myTransform.forward * movementSpeed * Time.deltaTime;
+    }
+}
