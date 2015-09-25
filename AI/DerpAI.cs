9171e243b9ff78447167eeee2207f9ea2a4e8eec
chdonncha <chdonncha@gmail.com>
2015-09-25 11:38:25 +0100

    Added very basic AI

---
+++ AI/DerpAI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DerpAI : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+    public Transform player;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
