d3bdd10a4f520bd8c4c58f9b202f0c02d8106f2a
chdonncha <chdonncha@gmail.com>
2015-09-25 15:40:57 +0100

    Added EnemyHealth

---
+++ Enemy/EnemyHealth.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public int currentHealth;
+    public int maxHealth;
+
+    public float hbLength;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentHealth = maxHealth;
+        hbLength = Screen.width / 6;
+        ChangeHealth(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(10, 50, hbLength, 25), currentHealth + " / " + maxHealth);
+
+    }
+
+    public void ChangeHealth(int health)
+    {
+        currentHealth += health;
+
+        hbLength = (Screen.width / 2) + (currentHealth / (float)maxHealth);
+    }
+}
